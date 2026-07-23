33f8b7683ada22a84ee5aaf98726291f13fac0b6
Anushka Gorla <anushkagorla02@gmail.com>
2026-07-23 23:40:25 +0530

    ✅ JAVAPROB03B | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-23

---
+++ codechef/medium/JAVAPROB03B/solution.java
@@ -0,0 +1,8 @@
+class Codechef {
+    public static void main(String[] args) {
+	    
+	    // Write the code below to print "Hello World!!"
+	    System.out.println("Hello World!!");
+	    
+    }
+}
