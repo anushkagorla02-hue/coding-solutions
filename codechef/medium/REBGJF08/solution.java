ff6b5f8931ccf1995df6451c0b224ed1f6389206
Anushka Gorla <anushkagorla02@gmail.com>
2026-07-23 23:52:33 +0530

    ✅ REBGJF08 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-23

---
+++ codechef/medium/REBGJF08/solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  // Print greeting message
+      System.out.println("Hello from Java!");
+      
+      // Print reminder about case sensitivity
+      System.out.println("Case is important!");
+      
+      // Print encouragement message
+      System.out.println("Keep Practicing.");
+	}
+}
