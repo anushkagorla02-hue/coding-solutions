68abcd5e91245aa2c93907530ee0805adb4cbaa8
Anushka Gorla <anushkagorla02@gmail.com>
2026-07-24 00:10:06 +0530

    ✅ REBGJF29 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-23

---
+++ codechef/medium/REBGJF29/solution.java
@@ -0,0 +1,7 @@
+ class Shopping
+{
+    public static void main(String[] args)
+    {
+        System.out.println("My shopping list:\n"+"\tBread\n"+"\t\"Organic\" Milk\n"+"\tApples\\Pears");
+    }
+}
