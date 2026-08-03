c039363467e47bc82e850e8f83c872770d0c9fce
Anushka Gorla <anushkagorla02@gmail.com>
2026-08-04 00:28:52 +0530

    ✅ LPYASM26 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-03

--- codechef/medium/LPYASM26/solution.py
+++ codechef/medium/LPYASM26/solution.py
@@ -1,10 +1,6 @@
-# cook your dish here
-t1=1430
-t2=1615
-h1=t1//100
-m1=t1%100
-tot1=(h1*60)+m1
-h2=t2//100
-m2=t2%100
-tot2=(h2*60)+m2
-print(tot2-tot1)
+# Debug the code given below
+temperature = 25
+
+# check
+result1 = (temperature >= 20) and (temperature <= 30)
+print(result1) 
