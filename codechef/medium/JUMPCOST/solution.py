7b100ddd449474e51fac791920b0c2160e1233dc
Anushka Gorla <anushkagorla02@gmail.com>
2026-08-05 21:31:59 +0530

    ✅ JUMPCOST | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-05

--- codechef/medium/JUMPCOST/solution.py
+++ codechef/medium/JUMPCOST/solution.py
@@ -1,10 +1,15 @@
 # cook your dish here
 t=int(input())
+arr=[]
 for _ in range(t):
     n=int(input())
     a=list(map(int,input().split()))
     for i in range(1,n):
         for j in range(i):
-            pos=a[i]-i+j
-            if a[pos]:
-                print(pos)
+            pos=a[i]-(i+1)+(j+1)
+            try:
+               if a[pos]:
+                  arr.append(pos)
+            except(IndexError):
+                arr=[]
+    print(len(arr))               
