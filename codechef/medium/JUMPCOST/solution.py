8b50dc2ce59619b60a81bc5bfc5c6e7b32ac8306
Anushka Gorla <anushkagorla02@gmail.com>
2026-08-05 21:14:44 +0530

    ✅ JUMPCOST | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-05

--- codechef/medium/JUMPCOST/solution.py
+++ codechef/medium/JUMPCOST/solution.py
@@ -1,1 +1,10 @@
 # cook your dish here
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    for i in range(1,n):
+        for j in range(i):
+            pos=a[i]-i+j
+            if a[pos]:
+                print(pos)
