3e08e51a8e2a7ab086476c68ca6128b5cb6cd809
Anushka Gorla <anushkagorla02@gmail.com>
2026-08-06 21:12:50 +0530

    ✅ LPYAS48 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-06

--- codechef/medium/LPYAS48/solution.py
+++ codechef/medium/LPYAS48/solution.py
@@ -1,3 +1,8 @@
-# Update the code below this line.
-a,b,c=input().split()
-print(a,"is",b,"in the",c)
+# Take two numbers in a single line separated by space
+one, two = input().split()   # You need to separate the inputs by spaces
+
+# Convert inputs to integers
+one = int(one)
+two = int(two)
+
+print(one // two)      # Expected output for input "5 5" is 1
