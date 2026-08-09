ef92626334afcc731d4e8d3846055ffefdca5223
Anushka Gorla <anushkagorla02@gmail.com>
2026-08-09 23:55:55 +0530

    ✅ DEBUGPYL9 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-09

--- codechef/medium/DEBUGPYL9/solution.py
+++ codechef/medium/DEBUGPYL9/solution.py
@@ -1,7 +1,4 @@
-#if condition is wrong it should be changed to n>5
-n = int(input())
+# Change the indexing from one based to zero based
+s = input() 
 
-if n > 5:
-    print("the number is greater than 5")
-else:
-    print("the number is smaller than or equal to 5")
+print(s[0] + s[3] + s[5])
