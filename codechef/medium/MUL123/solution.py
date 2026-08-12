e5ade0fe210ddf3cb9661bdca1f3ad6caadc6960
Anushka Gorla <anushkagorla02@gmail.com>
2026-08-12 21:50:46 +0530

    ✅ MUL123 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-12

--- codechef/medium/MUL123/solution.py
+++ codechef/medium/MUL123/solution.py
@@ -2,11 +2,11 @@
 t=int(input())
 for _ in range(t):
     n=int(input())
-    if n%5==0:
+    if n%2==0:
         print(1)
     elif n%3==0:
         print(0)
-    elif n%2==0:
+    elif n%5==0:
         print(1)
     else:
         print(2)
