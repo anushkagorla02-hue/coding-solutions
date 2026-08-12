5f10bda662238a78c5b2f11488589cacc2a1fbd5
Anushka Gorla <anushkagorla02@gmail.com>
2026-08-12 21:39:08 +0530

    ✅ CHKEV | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-12

--- codechef/medium/CHKEV/solution.py
+++ codechef/medium/CHKEV/solution.py
@@ -1,8 +1,8 @@
 # cook your dish here
 a,b=map(int,input().split())
-s="False"
-for i in range(a+1,b+1):
+s="No"
+for i in range(a+1,b):
     if a!=b or i%2==0:
-        s="True"
+        s="Yes"
         break
 print(s)
