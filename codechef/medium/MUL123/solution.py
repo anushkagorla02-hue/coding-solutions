b83170a27e70e82594a529422b245014112c205c
Anushka Gorla <anushkagorla02@gmail.com>
2026-08-12 21:58:00 +0530

    ✅ MUL123 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-12

--- codechef/medium/MUL123/solution.py
+++ codechef/medium/MUL123/solution.py
@@ -2,11 +2,9 @@
 t=int(input())
 for _ in range(t):
     n=int(input())
-    if n%2==0:
-        print(1)
-    elif n%3==0:
+    if n%3==0:
         print(0)
-    elif ((n//5+1)*5)%3==0:
+    elif n%3==2 or ((n//5+1)*5)%3==0:
         print(1)
     else:
         print(2)
