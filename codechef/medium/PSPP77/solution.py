6a0b3c2b87d46b01f4a4cbd54caa4b5faf057704
Anushka Gorla <anushkagorla02@gmail.com>
2026-08-14 21:09:07 +0530

    ✅ PSPP77 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-14

---
+++ codechef/medium/PSPP77/solution.py
@@ -0,0 +1,13 @@
+two = 2
+three = 3
+four = 4
+def square(i):
+    return i*i
+def cube(j):
+    return j**3
+print(square(two))
+print(cube(two))
+print(square(three))
+print(cube(three))
+print(square(four))
+print(cube(four))
