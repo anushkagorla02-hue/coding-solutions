299a75f6084c6b56e7aeaf4365609ee1d644eeac
Anushka Gorla <anushkagorla02@gmail.com>
2026-08-14 20:18:33 +0530

    ✅ PPSC94 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-14

--- codechef/medium/PPSC94/solution.py
+++ codechef/medium/PPSC94/solution.py
@@ -1,9 +1,9 @@
 # cook your dish here
-N = int(input())
-sum = 0
-
-for i in range(1, N + 1):
-    if i % 2 == 0:
-        sum = sum + i
-
+n=int(input())
+sum=0
+i=1
+while i<n:
+    if i%2==0:
+        sum+=i
+    i+=1
 print(sum)
