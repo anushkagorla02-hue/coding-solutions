f4017075c55b8a5ced077eb6e1b64d2e3228b946
Anushka Gorla <anushkagorla02@gmail.com>
2026-08-18 21:05:36 +0530

    ✅ LPYASM17 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-18

---
+++ codechef/medium/LPYASM17/solution.py
@@ -0,0 +1,12 @@
+# Update your code below
+person = {
+    "name": "John",
+    "age": 25,
+    "city": "New York",
+    "rating": 1550,
+    "nationality": "Indian"
+}
+list1=list(person.keys())
+list2=list(person.values())
+print(list1)
+print(list2)
