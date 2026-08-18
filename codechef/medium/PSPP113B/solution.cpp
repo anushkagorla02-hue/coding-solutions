8f514d189aa6f08a16d6140aed216124be226965
Anushka Gorla <anushkagorla02@gmail.com>
2026-08-18 20:57:10 +0530

    ✅ PSPP113B | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-18

---
+++ codechef/medium/PSPP113B/solution.cpp
@@ -0,0 +1,8 @@
+student = (["Alice", "Bob"], ["Math", "Science", "English"], 20, 'XYZ')
+
+# Update the code below this line
+list1,list2,num,string=student
+print(list1[1])
+print(list2[2])
+print(num)
+print(string[2])
