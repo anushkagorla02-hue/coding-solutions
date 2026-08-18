5b5b9d98907ea3dd18d43250194a2336263b847e
Anushka Gorla <anushkagorla02@gmail.com>
2026-08-18 22:36:38 +0530

    ✅ LJAAS10 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-18

--- codechef/medium/LJAAS10/solution.java
+++ codechef/medium/LJAAS10/solution.java
@@ -1,5 +1,9 @@
-class Codechef {
-    public static void main(String[] args) {
-        System.out.println(9*12);
-    }
+class Codechef
+{
+	public static void main (String[] args)
+	{
+        // Write your code here
+        System.out.println("Learn Coding on CodeChef");
+      
+	}
 }
