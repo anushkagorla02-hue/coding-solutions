33f8814c450cc49f14d9560f7f2c0add8d0f6c1e
Anushka Gorla <anushkagorla02@gmail.com>
2026-08-21 22:23:07 +0530

    ✅ GSQ06 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-21

--- codechef/medium/GSQ06/solution.sql
+++ codechef/medium/GSQ06/solution.sql
@@ -1,4 +1,8 @@
-/* Write a query to add the below mentioned employee details to the EMPLOYEE table.
-Employee id: 06, Employee Name: Brandon Kim, Department: Operations
-*/
-insert into Employee values(6,'Brandon Kim','Operations');
+/* The Query written in the console is trying to insert data to the table employee.
+Debug this query to output the entire table */
+
+INSERT INTO employee (Id,Name,Age,Address)
+VALUES  (1, 'John Smith', 25,  '123 Main St'),
+        (2, 'Sarah Johnson', 30,'456 Broadway');
+
+SELECT * FROM employee; 
