b07ab06a306a78d31612c33d1fa04ac3cc513cad
Anushka Gorla <anushkagorla02@gmail.com>
2026-08-21 22:22:04 +0530

    ✅ GSQ07 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-21

---
+++ codechef/medium/GSQ07/solution.cpp
@@ -0,0 +1,4 @@
+/* Write a query to insert the below mentioned employee details to the table 'employee' */
+
+insert into employee(employee_id,employee_name,department)
+values(4,'Marcus Garcia','Product'),(5,'Samantha Park','Hr');
