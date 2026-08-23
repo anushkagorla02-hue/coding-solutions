154c798c9e451b7e4f973423b311c2b34629f3bb
Anushka Gorla <anushkagorla02@gmail.com>
2026-08-23 23:39:08 +0530

    ✅ GSQ08 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-23

---
+++ codechef/medium/GSQ08/solution.sql
@@ -0,0 +1,3 @@
+/* Write a query to add a column 'Designation' to the table and set 'Null' as the default value. Output the entire table.*/
+alter table employee add column Designation text default Null;
+select *from employee;
