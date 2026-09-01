3024f08a9f1c0af2a2b14767d8810382dd51d7ae
Anushka Gorla <anushkagorla02@gmail.com>
2026-09-01 20:52:44 +0530

    ✅ PYPROJPR04 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-01

---
+++ codechef/medium/PYPROJPR04/solution.cpp
@@ -0,0 +1,20 @@
+        hint = giveHint(secretNumber, user_guess)
+        if hint == "Right":
+            print("You guessed it Right!")
+            break
+        else:
+            print(hint)
+            
+if __name__ == '__main__':
+    # Update the code below
+    while True:
+        user_guess = int(input("Enter a number between 1 and 100: "))
+def runGuess():
+    secretNumber = getRandomNumber()
+    else:
+        return "Hot"
+
+        return "Cold"
+    elif number == guess:
+        return "Right"
+    runGuess()
