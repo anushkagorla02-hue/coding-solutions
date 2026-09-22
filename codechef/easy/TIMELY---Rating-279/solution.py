de190dac2f7e6af4d1f3b8fa5dd2836fb372479f
Anushka Gorla <anushkagorla02@gmail.com>
2026-09-22 19:30:03 +0530

    ✅ TIMELY - Rating 279 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-22

--- codechef/easy/TIMELY---Rating-279/solution.py
+++ codechef/easy/TIMELY---Rating-279/solution.py
@@ -1,7 +1,3 @@
 # cook your dish here
 for _ in range(int(input())):
-    x=int(input())
-    if x in range(67,45001):
-        print("YES")
-    else:
-        print("NO")
+    print("YES" if int(input())>=30 else "NO")
